76db740b5788e6d89573438907ec2788fb15b9b8
Harriet Oteng <harriet.oteng@gmail.com>
2024-01-17 19:23:52 +0000

    Filter and display possible searched city

--- src/componets/HomePage.js
+++ src/componets/HomePage.js
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { IoMdSearch } from "react-icons/io";
 import HomePageNav from './HomePageNav';
+import { cities } from '../data/cities';
 
 const HomePage = () => {
   const [searchTerm, setSearchTerm] = useState('');
+
+  const matched = cities.filter((city) => city.name.toLowerCase().includes(searchTerm.trim().toLowerCase()));
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    if (searchTerm !== '') {
+    }
+    console.log(matched)
+  }
+
+  const handleClick = (e) => {
+    console.log(matched);
+  }
 
   return (
     <main className=''>
       <HomePageNav />
       <div className='bg-castle h-[400px] bg-cover py-[30px]'>
         <div className='bg-black/65 max-w-screen-xl mx-auto p-[50px]'>
-          <form className='bg-white flex'>
+          <form onSubmit={handleSumbit} className='bg-white flex'>
             <input
               name='searchTerm'
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               placeholder='Search for a place'
-              type='search'
+              type='text'
               className='outline-none w-full px-[24px] py-[8px]'
             />
             <button
@@ -26,9 +40,20 @@ const HomePage = () => {
                 <IoMdSearch className='text-gray-600 text-[32px]' />
               </button>
           </form>
-          <div>
-            Display matching cities
-          </div>
+          {searchTerm.trim().length !== 0 && (
+            <div className='text-gray-600 bg-white mt-2'>
+              {matched.map((city, index) => (
+                <div
+                  id={index}
+                  key={index}
+                  onClick={handleClick}
+                  className='border p-2 cursor-pointer'
+                >
+                  {city.name}{' '}({city.cou_name_en})
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </main>
