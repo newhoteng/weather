ae1b8dfe8cd90b2305687e064baeec14bb09b8f0
Harriet Oteng <harriet.oteng@gmail.com>
2024-01-17 11:48:38 +0000

    Style search bar

--- src/componets/HomePage.js
+++ src/componets/HomePage.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdSearch } from "react-icons/io";
 import HomePageNav from './HomePageNav';
 
 const HomePage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main className=''>
       <HomePageNav />
-      <div className='bg-plane'>
-        <form className='border border-red-600'>
+      <div className='bg-castle h-[400px] bg-cover py-[30px]'>
+        <div className='bg-black/65 max-w-screen-xl mx-auto p-[50px]'>
+          <form className='bg-white flex'>
+            <input
+              name='searchTerm'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder='Search for a place'
+              type='search'
+              className='outline-none w-full px-[24px] py-[8px]'
+            />
+            <button
+                type='submit'
+                className='basis-1/12 flex items-center justify-center'
+              >
+                <IoMdSearch className='text-gray-600 text-[32px]' />
+              </button>
+          </form>
           <div>
-            <input placeholder='Search for a place' type='search' />
-            <IoMdSearch />
+            Display matching cities
           </div>
-        </form>
+        </div>
       </div>
     </main>
   )
